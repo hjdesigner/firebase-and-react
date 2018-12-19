152eb223099ee098e69f9ee93ba4cae2466de2cd
Henrique Melanda <hjdesigner23@gmail.com>
2018-12-19 01:03:15 -0200

    Autenticacao de usuario com o google

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import firebase from './firebase';
+import firebase, { auth, provider }  from './firebase';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +8,13 @@ class App extends Component {
     this.state = {
       currentItem: '',
       username: '',
-			items: []
+			items: [],
+			user: null
 		}
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.login = this.login.bind(this);
+		this.logout = this.logout.bind(this);
 	}
 	handleChange(e) {
 		this.setState({
@@ -23,7 +26,7 @@ class App extends Component {
 		const itemsRef = firebase.database().ref('items');
 		const item = {
     	title: this.state.currentItem,
-    	user: this.state.username
+    	user: this.state.user.displayName || this.state.user.email
   	}
   	itemsRef.push(item);
   	this.setState({
@@ -34,8 +37,31 @@ class App extends Component {
 	removeItem(itemId) {
     const itemRef = firebase.database().ref(`/items/${itemId}`);
     itemRef.remove();
-  }
+	}
+	logout() {
+		auth.signOut()
+    	.then(() => {
+      	this.setState({
+       		user: null
+      	});
+    	});
+	}
+	login() {
+  	auth.signInWithPopup(provider) 
+    	.then((result) => {
+      	const user = result.user;
+      	this.setState({
+        	user
+      	});
+    	});
+	}
 	componentDidMount() {
+		auth.onAuthStateChanged((user) => {
+    	if (user) {
+      	this.setState({ user });
+    	} 
+  	});
+
   	const itemsRef = firebase.database().ref('items');
   	itemsRef.on('value', (snapshot) => {
     	let items = snapshot.val();
@@ -59,10 +85,25 @@ class App extends Component {
         <header>
             <div className='wrapper'>
               <h1>Fun Food Friends</h1>
-              
+              {this.state.user ?
+    						<button onClick={this.logout}>Log Out</button>                
+						    :
+						    <button onClick={this.login}>Log In</button>              
+						  }
             </div>
-        </header>
-        <div className='container'>
+			</header>
+			{this.state.user ?
+	    	<div>
+		      <div className='user-profile'>
+    		    <img src={this.state.user.photoURL} />
+		      </div>
+    		</div>
+    		:
+		    <div className='wrapper'>
+    		  <p>You must be logged in to see the potluck list and submit to it.</p>
+		    </div>
+		  }
+			<div className='container'>
           <section className='add-item'>
               <form onSubmit={this.handleSubmit}>
                 <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
@@ -76,10 +117,12 @@ class App extends Component {
 								{this.state.items.map((item) => {
         					return (
 					          <li key={item.id}>
-          					  <h3>{item.title}</h3>
-											<p>brought by: {item.user}</p>
-											<button onClick={() => this.removeItem(item.id)}>Remove Item</button>
-          					</li>
+											<h3>{item.title}</h3>
+											<p>brought by: {item.user}
+			                	{item.user === this.state.user.displayName || item.user === this.state.user.email ?
+                   			<button onClick={() => this.removeItem(item.id)}>Remove Item</button> : null}
+				              </p>
+										</li>
 					        )
 					      })}
               </ul>
